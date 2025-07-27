3af514003b5615bba54724fd0e24cccf734f0634
bald-freak <fragafonov@gmail.com>
2025-07-28 04:38:24 +1200

    Update my_wallpaper.js

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,11 +1,10 @@
 //your parameter variables go here!
-let rect_width  = 20;
-let rect_height = 30;
-
+let centre_diameter = 50
+let centre_coord = 100
 
 function setup_wallpaper(pWallpaper) {
-  pWallpaper.output_mode(DEVELOP_GLYPH);
-  //pWallpaper.output_mode(GRID_WALLPAPER);
+  //pWallpaper.output_mode(DEVELOP_GLYPH);
+  pWallpaper.output_mode(GRID_WALLPAPER);
   
   pWallpaper.resolution(FIT_TO_SCREEN);
   pWallpaper.show_guide(true); //set this to false when you're ready to print
@@ -17,9 +16,25 @@ function setup_wallpaper(pWallpaper) {
 }
 
 function wallpaper_background() {
-  background(240, 255, 240); //light honeydew green colour
+  background(235, 233, 204); //beige colour
 }
 
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
-  rect(40 ,40, rect_width, rect_height);
+
+  //petals
+  stroke(69, 15, 46)//purple
+  fill(110, 29, 49)//red
+  //triangle(160, 150, 170, 140, 180, 150)
+  ellipse(centre_coord, centre_coord, centre_diameter + 120, centre_diameter - 10)
+  ellipse(centre_coord, centre_coord, centre_diameter - 10, centre_diameter + 120)
+  //centre
+  fill(168, 133, 99)//brown
+  stroke(135, 76, 36)//dark brown
+  strokeWeight(2)
+  ellipse(centre_coord, centre_coord, centre_diameter + 20, centre_diameter + 20)
+  strokeWeight(14)
+  stroke(209, 158, 40)//orange
+  fill(240, 234, 125)//yellow
+  ellipse(centre_coord, centre_coord, centre_diameter, centre_diameter)
 }
+
